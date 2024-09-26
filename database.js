c3bcaed83a73111f8efb5fdd023d8f9b440f7d95
Ori13598 <maibason13598@gmail.com>
2024-09-26 15:14:12 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,160 +1,205 @@
-/*Generate HTML SIGHTSEEING*/
-  let sightseeingHTML="";
-  let array_sightseeing_carousel =[];
-  let array_sightseeing_slideindex =[];
-  let a = 1;
-  DB_sightseeing_JAN.forEach((sightseeing_JAN) => {
-    sightseeingHTML +=`
-          <div class="MID_travelspot">
-            <div class="MID_travelspot_carousel_sightseeing${a} fade">
-                <img class="MID_travelspot_image_img" src=${DB_sightseeing_JAN[`${a-1}`].image1}>
-                <p class ="MID_travelspot_infor_tag">${DB_sightseeing_JAN[`${a-1}`].content1.jpcontent}</p>
-            </div>
-            <div class="MID_travelspot_carousel_sightseeing${a} fade">
-                <img class="MID_travelspot_image_img" src="${DB_sightseeing_JAN[`${a-1}`].image2}">
-              <p class ="MID_travelspot_infor_tag">${DB_sightseeing_JAN[`${a-1}`].content2.jpcontent}</p>
-            </div>
-            <div class="MID_travelspot_carousel_sightseeing${a} fade">
-                <img class="MID_travelspot_image_img" src="${DB_sightseeing_JAN[`${a-1}`].image3}">
-              <p class ="MID_travelspot_infor_tag">${DB_sightseeing_JAN[`${a-1}`].content3.jpcontent}</p>
-            </div>
-            <img class="MID_travelspot_image_gif" src="image/Apple.gif">
-            <a class="prev prevsightseeing" onclick="plusSlides_sightseeing(-1, ${a-1})">❮</a>
-            <a class="next nextsightseeing" onclick="plusSlides_sightseeing(1, ${a-1})">❯</a>
-          </div>`;
-    array_sightseeing_carousel.push(`MID_travelspot_carousel_sightseeing${a}`);
-    array_sightseeing_slideindex.push(1);
-    a++;
-  });
-  document.querySelector(".MID_content_sightseeing").innerHTML = sightseeingHTML;
-  
+/* monthly database for sightseeing*/
+const DB_sightseeing_JAN = [{
+    image1:"image/02.MID/01.Jan/01.sightseeing/01.png",
+    content1:{
+        encontent:"Yamamoto teacher",
+        jpcontent:"山本先生"
+    },
+    image2:"image/02.MID/01.Jan/01.sightseeing/02.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"青森での冬が絶景だ"
+    },
+    image3:"image/02.MID/01.Jan/01.sightseeing/03.png",
+    content3:{
+        encontent:"aomori has a lot of snows",
+        jpcontent:"青森では雪がよく振っている"
+    },
+},{
+    image1:"image/02.MID/01.Jan/01.sightseeing/03.png",
+    content1:{
+        encontent:"There is a lot of snow piled up in Aomori",
+        jpcontent:"青森での雪がたくさん積んでいる"
+    },
+    image2:"image/02.MID/01.Jan/01.sightseeing/02.png",
+    content2:{
+        encontent:"There is a great sightseeing of Aomori",
+        jpcontent:"青森の景色が綺麗だ"
+    },
+    image3:"image/02.MID/01.Jan/01.sightseeing/04.png",
+    content3:{
+        encontent:"Great picture",
+        jpcontent:"綺麗な写真だ"
+    },
+}];
+const DB_sightseeing_FEB = [{
+    image1:"image/02.MID/02.Feb/01.sightseeing/01.png",
+    content1:{
+        encontent:"February",
+        jpcontent:"2月"
+    },
+    image2:"image/02.MID/02.Feb/01.sightseeing/02.png",
+    content2:{
+        encontent:"Higashida-kun",
+        jpcontent:"東田さん"
+    },
+    image3:"image/02.MID/02.Feb/01.sightseeing/03.png",
+    content3:{
+        encontent:"There is a lot of snows",
+        jpcontent:"雪がたくさん"
+    },
+},{
+    image1:"image/02.MID/02.Feb/01.sightseeing/03.png",
+    content1:{
+        encontent:"Local people recommendation",
+        jpcontent:"地元のおすすめ"
+    },
+    image2:"image/02.MID/02.Feb/01.sightseeing/04.png",
+    content2:{
+        encontent:"MAI BA SON",
+        jpcontent:"マイバソン"
+    },
+    image3:"image/02.MID/02.Feb/01.sightseeing/02.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+}];
 
-/*Generate HTML FOODTOUR*/
+/* monthly database for foodtour*/
+const DB_foodtour_JAN = [{
+    image1:"image/02.MID/01.Jan/02.foodtour/01.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/01.Jan/02.foodtour/02.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/01.Jan/02.foodtour/04.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+},{
+    image1:"image/02.MID/01.Jan/02.foodtour/03.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/01.Jan/02.foodtour/04.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/01.Jan/02.foodtour/01.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+}];
+const DB_foodtour_FEB = [{
+    image1:"image/02.MID/02.Feb/02.foodtour/01.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/02.Feb/02.foodtour/02.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/02.Feb/02.foodtour/03.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+},{
+    image1:"image/02.MID/02.Feb/02.foodtour/03.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/02.Feb/02.foodtour/04.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/02.Feb/02.foodtour/02.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+}];
 
-  let foodtourHTML="";
-  let array_foodtour_carousel =[];
-  let array_foodtour_slideindex =[];
-  let b = 1;
-  DB_foodtour_JAN.forEach((foodtour_JAN) => {
-    foodtourHTML +=`
-            <div class="MID_travelspot">
-              <div class="MID_travelspot_carousel_foodtour${b} fade">
-                <img class="MID_travelspot_image_img" src="${DB_foodtour_JAN[`${b-1}`].image1}">
-                <p class ="MID_travelspot_infor_tag">${DB_foodtour_JAN[`${b-1}`].content1.jpcontent}</p>
-             </div>
-              <div class="MID_travelspot_carousel_foodtour${b} fade">
-                <img class="MID_travelspot_image_img" src="${DB_foodtour_JAN[`${b-1}`].image2}">
-                <p class ="MID_travelspot_infor_tag">${DB_foodtour_JAN[`${b-1}`].content2.jpcontent}</p>
-              </div>
-              <div class="MID_travelspot_carousel_foodtour${b} fade">
-                <img class="MID_travelspot_image_img" src="${DB_foodtour_JAN[`${b-1}`].image3}">
-                <p class ="MID_travelspot_infor_tag">${DB_foodtour_JAN[`${b-1}`].content3.jpcontent}</p>
-              </div>
-              <a class="prev prevfoodtour" onclick="plusSlides_foodtour(-1, ${b-1})">❮</a>
-              <a class="next nextfoodtour" onclick="plusSlides_foodtour(1, ${b-1})">❯</a>
-            </div>`;
-    array_foodtour_carousel.push(`MID_travelspot_carousel_foodtour${b}`);
-    array_foodtour_slideindex.push(1);
-    b++;
-  });
-  document.querySelector(".MID_content_foodtour").innerHTML = foodtourHTML;
-  
+/* monthly database for entertainment*/
+const DB_entertainment_JAN = [{
+    image1:"image/02.MID/01.Jan/03.entertainment/01.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/01.Jan/03.entertainment/02.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/01.Jan/03.entertainment/03.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+},{
+    image1:"image/02.MID/01.Jan/03.entertainment/03.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/01.Jan/03.entertainment/04.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/01.Jan/03.entertainment/02.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+}];
+const DB_entertainment_FEB = [{
+    image1:"image/02.MID/02.Feb/03.entertainment/01.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/02.Feb/03.entertainment/02.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/02.Feb/03.entertainment/02.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+},{
+    image1:"image/02.MID/02.Feb/03.entertainment/03.png",
+    content1:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image2:"image/02.MID/02.Feb/03.entertainment/04.png",
+    content2:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+    image3:"image/02.MID/02.Feb/03.entertainment/02.png",
+    content3:{
+        encontent:"aomori has the beautiful winter",
+        jpcontent:"日本語が難しい"
+    },
+}];
 
-/*Generate HTML ENTERTAINMENT*/
-  let entertainmentHTML="";
-  let array_entertainment_carousel = [];
-  let array_entertainment_slideindex = [];
-  let c = 1;
-  DB_entertainment_JAN.forEach((entertainment_JAN) => {
-    entertainmentHTML +=`
-            <div class="MID_travelspot">
-              <div class="MID_travelspot_carousel_entertainment${c} fade">
-                  <img class="MID_travelspot_image_img" src="${DB_entertainment_JAN[`${c-1}`].image1}">
-                <p class ="MID_travelspot_infor_tag">${DB_entertainment_JAN[`${c-1}`].content1.jpcontent}</p>
-              </div>
-              <div class="MID_travelspot_carousel_entertainment${c} fade">
-                <img class="MID_travelspot_image_img" src="${DB_entertainment_JAN[`${c-1}`].image2}">
-                <p class ="MID_travelspot_infor_tag">${DB_entertainment_JAN[`${c-1}`].content2.jpcontent}</p>
-              </div>
-              <div class="MID_travelspot_carousel_entertainment${c} fade">
-                <img class="MID_travelspot_image_img" src="${DB_entertainment_JAN[`${c-1}`].image3}">
-                <p class ="MID_travelspot_infor_tag">${DB_entertainment_JAN[`${c-1}`].content3.jpcontent}</p>
-              </div>
-              <a class="prev preventertainment" onclick="plusSlides_entertainment(-1, ${c-1})">❮</a>
-              <a class="next nextentertainment" onclick="plusSlides_entertainment(1, ${c-1})">❯</a>
-            </div>`;
-    array_entertainment_carousel.push(`MID_travelspot_carousel_entertainment${c}`);
-    array_entertainment_slideindex.push(1);
-    c++;
-  });
-  document.querySelector(".MID_content_entertainment").innerHTML = entertainmentHTML;
-/*Function slideshow for sightseeing area*/
-let k = 0;
-array_sightseeing_slideindex.forEach(() =>{
-  showSlides_sightseeing(1, k);
-  k++;
-});
-
-
-function plusSlides_sightseeing(n, no) {
-  showSlides_sightseeing(array_sightseeing_slideindex[no] += n, no);
-}
-
-function showSlides_sightseeing(n, no) {
-  let i;
-  let x = document.getElementsByClassName(array_sightseeing_carousel[no]);
-  if (n > x.length) {array_sightseeing_slideindex[no] = 1}    
-  if (n < 1) {array_sightseeing_slideindex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[array_sightseeing_slideindex[no]-1].style.display = "block";  
-}
-
-
-/*Function slideshow for foodtour area*/
-let h = 0;
-array_foodtour_slideindex.forEach(() =>{
-  showSlides_foodtour(1, h);
-  h++;
-});
-
-
-function plusSlides_foodtour(n, no) {
-  showSlides_foodtour(array_foodtour_slideindex[no] += n, no);
-}
-
-function showSlides_foodtour(n, no) {
-  let i;
-  let x = document.getElementsByClassName(array_foodtour_carousel[no]);
-  if (n > x.length) {array_foodtour_slideindex[no] = 1}    
-  if (n < 1) {array_foodtour_slideindex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[array_foodtour_slideindex[no]-1].style.display = "block";  
-}
-
-/*Function slideshow for foodtour area*/
-let j = 0;
-array_entertainment_slideindex.forEach(() =>{
-  showSlides_entertainment(1, j);
-  j++;
-});
-
-
-function plusSlides_entertainment(n, no) {
-  showSlides_entertainment(array_entertainment_slideindex[no] += n, no);
-}
-
-function showSlides_entertainment(n, no) {
-  let i;
-  let x = document.getElementsByClassName(array_entertainment_carousel[no]);
-  if (n > x.length) {array_entertainment_slideindex[no] = 1}    
-  if (n < 1) {array_entertainment_slideindex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[array_entertainment_slideindex[no]-1].style.display = "block";  
-}
 
